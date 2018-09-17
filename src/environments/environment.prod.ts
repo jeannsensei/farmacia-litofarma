29d357754676a82086aea08c824e3752f2b53427
Jeannuel Garcia <jjeanlink@gmail.com>
2018-09-17 01:10:34 -0500

    usando firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+    firebaseConfig: {
+    apiKey: 'AIzaSyBCVij-f-Of1E4huM2JELeTFAEkqFcw1y0',
+    authDomain: 'tienda-2fcbe.firebaseapp.com',
+    databaseURL: 'https://tienda-2fcbe.firebaseio.com',
+    projectId: 'tienda-2fcbe',
+    storageBucket: 'tienda-2fcbe.appspot.com',
+    messagingSenderId: '643332586182'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig: {
     apiKey: 'AIzaSyBCVij-f-Of1E4huM2JELeTFAEkqFcw1y0',
     authDomain: 'tienda-2fcbe.firebaseapp.com',
